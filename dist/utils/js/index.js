d3807518a4dc03b578613456383089e8244992b7
artadmire <artadmire@163.com>
2019-01-08 16:50:58 +0800

    插件

--- dist/utils/js/index.js
+++ dist/utils/js/index.js
@@ -1,44 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-
-var _index2 = require('../../utils/js/index');
-
-// import  unslider  from "./lunbo"
-
-$(function () {
-    //轮播图
-    // unslider();
-
-    $(document).ready(function (e) {
-        var unslider04 = $('#b04').unslider({
-            dots: true
-        }),
-            data04 = unslider04.data('unslider');
-
-        $('.unslider-arrow04').click(function () {
-            var fn = this.className.split(' ')[1];
-            data04[fn]();
-        });
-    });
-    var arr = [3, 2, 3, 5, 5, 4, 6, 75, 3, 3, 24, 3];
-    (0, _index2.norepeat)(arr);
-    console.log(arr);
-    //tab切换
-    $('.pro-liste .list').eq(0).show().siblings().hide();
-    $('.pro-sort div').on('click', function () {
-        var _index = $(this).index();
-        $(this).addClass("active").siblings().removeClass("active");
-        $('.pro-list .list').eq(_index).show().siblings().hide();
-    });
-
-    $('.modle-content>div').eq(0).show().siblings().hide();
-    $('.modle-title ul li').on('click', function () {
-        var _index = $(this).index();
-        $(this).addClass("active").siblings().removeClass("active");
-        $('.modle-content>div').eq(_index).show().siblings().hide();
-    });
-});
-},{"../../utils/js/index":2}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -79,6 +39,4 @@ function norepeat(arr) {
     }
   }
 }
-
-
 },{}]},{},[1])
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,39 @@
+
 var gulp = require('gulp');
-//编译sass插件
+// 编译sass插件
 var sass = require('gulp-sass');
-//实时刷新html页面
+// 实时刷新html页面
 var connect = require('gulp-connect');
-//压缩css文件
+// 压缩css文件
 var cleanCSS = require('gulp-clean-css');
-//压缩html文件
+// 压缩html文件
 var htmlmin = require('gulp-htmlmin');
-//压缩js文件插件
+// 压缩js文件插件
 var uglify = require('gulp-uglify');
-//es6转es5
+// es6转es5
 var babel = require('gulp-babel');
 var babelCore = require("babel-core");
-//编译sass的任务
+var browserify = require("gulp-browserify");
+// 编译sass的任务
 gulp.task("sass", function () {
-    return gulp.src("src/**/css/*.scss")
+     gulp.src("src/**/css/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("dist/"))
 })
-//Es6转换es5
+// Es6转换es5
 gulp.task("babel", function () {
-    return gulp.src("src/**/js/*.js")
+     gulp.src("src/**/js/*.js")
         .pipe(babel({
             presets: ['es2015']
         }))
+        .pipe(browserify({
+            nobuiltins: 'events querystring'
+          }))
         .pipe(gulp.dest("dist/"))
 })
-//刷新页面
+// 刷新页面
 gulp.task('html',function(){
-     return gulp.src('src/**/index.html')
+      gulp.src('src/**/index.html')
      .pipe(connect.reload())})
 //定义livereload任务,实时刷新页面不可缺少
 gulp.task('connect', function () {
@@ -38,16 +43,18 @@ gulp.task('connect', function () {
 })
 // 压缩html文件
 gulp.task("htmlmin",function () {
-    return gulp.src("src/**/index.html")
+     gulp.src("src/**/index.html")
     .pipe(htmlmin({
         collapseWhitespace: true
     }))
     .pipe(gulp.dest("dist/"))
 })
+
 // 监听任务，文件夹发生变化 立即执行任务
 gulp.task("watch", function () {
-    gulp.watch("src/**/css/*.scss", ["sass"])
-    gulp.watch("src/**/js/*js",["babel"])
-    gulp.watch('src/**/index.html', ['html',"htmlmin"]);  //监听html目录下所有文件
+    // 检测到文件变化就会执行后面的任务
+    gulp.watch("src/**/css/*.scss", ["sass","htmlmin",'html'])
+    gulp.watch("src/**/js/*js",["babel","htmlmin",'html'])
+    gulp.watch('src/**/index.html', ["htmlmin",'html']);  //监听html目录下所有文件
 })
-gulp.task('dev', ['watch',"html","connect"])
+gulp.task("dev", ["watch","connect"])
